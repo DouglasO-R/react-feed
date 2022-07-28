286c8dc371521893623db1f8eea72fcddc4e3e8a
douglasor <douglasor@outlook.com>
2022-07-28 16:14:52 -0300

    Add Comment Component in Post Component

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img className={styles.avatar} src="https://github.com/DouglasO-R.png" alt="" />
+
+            <div className={styles.commentBox}>
+
+                <div className={styles.commentContent}>
+
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Douglas Oliveira</strong>
+                            <time title="28 de julho as 13:59" dateTime="2022-07-28 13:59:00"> Publicado ha 1 hora</time>
+                        </div>
+
+                        <button title="excluir">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Applaudir <span> 20 </span>
+                    </button>
+                </footer>
+
+            </div>
+
+        </div>
+    );
+}
